4b4e2a306123914e369fe6b5223fb4051bb527b9
XuJiajia <jiajia.xu@kdbund.com>
2018-08-06 18:45:30 +0800

    fix the bug for freight caculate and add funcation for fuel surcharge caculate

--- model/pages/getCargo_page.py
+++ model/pages/getCargo_page.py
@@ -29,7 +29,8 @@ class getCargo():
     table_loc = (By.ID, '07f19511-22e8-4579-824f-ec06d98ad514-non-body')
 
     def __init__(self):
-        self.client = MongoClient('192.168.1.79', 27017, connect=False)  # 防止出现no servers found yet错误
+        # self.client = MongoClient('192.168.1.79', 27017, connect=False)  # 防止出现no servers found yet错误
+        self.client = MongoClient('47.75.132.197', 27017, connect=False)  # 防止出现no servers found yet错误
         db = self.client.LdxSmart
         self.packageItem = db.packageItem
         self.productInfo = db.productInfo
@@ -39,6 +40,7 @@ class getCargo():
         self.charging = db.charging
         self.productCustomer = db.productCustomer
         self.freightInfo = db.freightInfo
+        self.chargeConfig = db.chargeConfig
 
     #   打开页面
     def enter(self, driver):
@@ -97,9 +99,9 @@ class getCargo():
         time.sleep(2)
 
     #   选择货物类型
-    def cargotype_select(self, driver, cargoType):
+    def cargotype_select(self, driver, index):
         try:
-            select.Select(driver.find_element(*self.cargoType_loc)).select_by_visible_text(cargoType)
+            select.Select(driver.find_element(*self.cargoType_loc)).select_by_index(index)
         except Exception as e:
             print e
         time.sleep(2)
@@ -219,138 +221,232 @@ class getCargo():
             print e
         time.sleep(3)
 
-    #   计算运费
-    def freight_caculate(self, waybill):
-        # 获取当前运费和燃油附加费
-        waybill_id = self.packageItem.find_one({'waybill_no': waybill})['_id']
-        account_id = self.accountInfo.find_one({'waybill_id': waybill_id})['_id']
-        freight = self.charging.find_one({'account_id': account_id, 'item': '运费'})['fee']
-        fuel_surcharge = self.charging.find_one({'account_id': account_id, 'item': '燃油附加费'})['fee']
+    # 判断是否有运费和燃油附加费的费用配置
+    def fee_config(self, waybill):
+        site_id = self.packageItem.find_one({'waybill_no': waybill})['current_site']
+        freight_config = self.chargeConfig.find_one({'site_id': site_id, 'charge_item': "运费"})
 
-        # 获取正确运费和燃油附加费
-        customer_id = self.packageItem.find_one({'waybill_no': waybill})['customer_id']
-        product_id = self.packageItem.find_one({'waybill_no': waybill})['product_id']
-        try:
-            #  产品客户有关联,取产品客户关联id
-            product_customer_id = self.productCustomer.find_one({'product_id': product_id,
+    # 计算运费
+    def freight_caculate(self, waybill):
+
+        # 判断该站点是否有运费的费用配置
+        site_id = self.packageItem.find_one({'waybill_no': waybill})['current_site']
+        freight_config = self.chargeConfig.find_one({'site_id': site_id, 'charge_item': "运费"})
+        freight = 0
+        if not freight_config:
+            # 获取当前运费
+            try:
+                waybill_id = self.packageItem.find_one({'waybill_no': waybill})['_id']
+                account_id = self.accountInfo.find_one({'waybill_id': waybill_id})['_id']
+                freight = self.charging.find_one({'account_id': account_id, 'item': '运费'})['fee']
+
+                # 获取正确运费和燃油附加费
+                customer_id = self.packageItem.find_one({'waybill_no': waybill})['customer_id']
+                product_id = self.packageItem.find_one({'waybill_no': waybill})['product_id']
+            except Exception as e:
+                print e
+            try:
+                #  产品客户有关联,取产品客户关联id
+                product_customer_id = self.productCustomer.find_one({'product_id': product_id,
                                                             'customer_id': customer_id})['_id']
-        except Exception as e:
-            print e
-            #  产品客户无关联,取产品的默认id
-            product_customer_id = self.productCustomer.find_one({'product_id': product_id,
+            except Exception as e:
+                print e
+                #  产品客户无关联,取产品的默认id
+                product_customer_id = self.productCustomer.find_one({'product_id': product_id,
                                                             'customer_id': None})['_id']
 
-        # 获取结算重量
-        account_weight = self.accountInfo.find_one({'waybill_id': waybill_id})['account_weight']
-        # 获取货物类型
-        now_cargo_type = self.packageItem.find_one({'waybill_no': waybill})['cargo_type']
-        cargo_type = 0  # packageItem中cargo_type值与freightInfo中不一致
-        if now_cargo_type == 0:
-            cargo_type = 1
-        else:
-            cargo_type = 2
+            # 获取结算重量
+            account_weight = self.accountInfo.find_one({'waybill_id': waybill_id})['account_weight']
+            # 获取货物类型
+            now_cargo_type = self.packageItem.find_one({'waybill_no': waybill})['cargo_type']
+            cargo_type = 0  # packageItem中cargo_type值与freightInfo中不一致
+            if now_cargo_type == 0:
+                cargo_type = 1
+            else:
+                cargo_type = 2
 
-        #  判断freightInfo中是否有货物类型,若无该值则使用默认报价
-        ori = []
-        des = []
-        try:
-            for ori_weight in self.freightInfo.find({'productCustomer_id': product_customer_id,
-                                                         'cargo_type': cargo_type})['ori_weight']:
-                print ori_weight
-                ori.append(ori_weight)
-            for des_weight in self.freightInfo.find({'productCustomer_id': product_customer_id,
-                                                         'cargo_type': cargo_type})['des_weight']:
-                print des_weight
-                des.append(des_weight)
-        except Exception as e:
-            print e
-            cargo_type = None
-            for ori_weight in self.freightInfo.find({'productCustomer_id': product_customer_id}):
-                print ori_weight
-                ori.append(ori_weight['ori_weight'])
-            for des_weight in self.freightInfo.find({'productCustomer_id': product_customer_id}):
-                print des_weight
-                des.append(des_weight['des_weight'])
+            #  判断freightInfo中是否有货物类型,若无该值则使用默认报价
+            ori = []
+            des = []
+            ori_weight_num = self.freightInfo.find({'productCustomer_id': product_customer_id, 'cargo_type': cargo_type})
+            if not ori_weight_num:
+                for ori_weight in self.freightInfo.find({'productCustomer_id': product_customer_id, 'cargo_type': cargo_type}):
+                    ori.append(ori_weight['ori_weight'])
 
-        weight = 0
-        continue_weight = 0
-        account_type = 0
-        first_fee = 0
-        price = 0
-        for i in range(0, len(ori)):
-            if ori[i] < account_weight <= des[i]:
-                try:
-                    account_type = self.freightInfo.find_one({'productCustomer_id': product_customer_id,
-                                                              'ori_weight': ori[i], 'des_weight': des[i]
-                                                              })['account_type']  # 结算类型
-                    first_fee = self.freightInfo.find_one({'productCustomer_id': product_customer_id, 'account_type': 1,
-                                                           'cargo_type': cargo_type})['unit_price']  # 首重价格
-                    first_weight = self.freightInfo.find_one({'productCustomer_id': product_customer_id,
-                                                              'account_type': 1, 'cargo_type': cargo_type})['des_weight']  # 首重
-                    continue_weight = account_weight - first_weight
-                except Exception as e:
-                    print e
-
-                if account_type == 1:  # 首重
-                    if first_fee == freight:
-                        return True
-                    else:
-                        return False
-
-                elif account_type == 2:  # 一倍续重
-                    weight = math.ceil(continue_weight)  # 续重部分1倍向上取整
-                    continue_price = self.freightInfo.find_one({'productCustomer_id': product_customer_id,
-                                                              'cargo_type': cargo_type, 'account_type': 2
-                                                              })['unit_price']  # 1倍续重单价
-                    continue_fee = weight * continue_price  # 续重价格
-                    all_fee = first_fee + continue_fee
-                    if all_fee == freight:
-                        return True
-                    else:
-                        return False
-
-                elif account_type == 3:  # 0.5倍续重
-
-                    multiple = (continue_weight // 1) / 0.5
-
-                    remainder = continue_weight % 1
-                    if remainder <= 0.5:
-                        multiple += 1
-                    else:
-                        multiple += 2
-
-                    continue_price = self.freightInfo.find_one({'productCustomer_id': product_customer_id,
-                                                            'cargo_type': cargo_type, 'account_type': 3})['unit_price']  # 0.5倍续重单价
-                    continue_fee = continue_price * multiple  # 续重价格
-                    all_fee = first_fee + continue_fee
-                    if all_fee == freight:
-                        return True
-                    else:
-                        return False
-
-                elif account_type == 4:  # 单价
-                    weight = math.ceil(account_weight)  # 结算重量1倍向上取整
-                    try:
-                        price = self.freightInfo.find_one({'productCustomer_id': product_customer_id, 'account_type': 4,
-                                                           'cargo_type': cargo_type})['unit_price']  # 单价
-                    except Exception as e:
-                        print e
-                    all_fee = weight * price
-                    if all_fee == freight:
-                        return True
-                    else:
-                        return False
-
-                elif account_type == 5:  # 固定价格
-                    price = self.freightInfo.find_one({'productCustomer_id': product_customer_id,
-                                                              'cargo_type': cargo_type, 'account_type': 5
-                                                              })['unit_price']  # 固定价格
-                    if price == freight:
-                        return True
-                    else:
-                        return False
+                for des_weight in self.freightInfo.find({'productCustomer_id': product_customer_id, 'cargo_type': cargo_type}):
+                    des.append(des_weight['des_weight'])
 
             else:
-                print weight
+                print u'该货物类型无报价,使用产品客户关联默认报价'
+                cargo_type = None
+                for ori_weight in self.freightInfo.find({'productCustomer_id': product_customer_id}):
+                    ori.append(ori_weight['ori_weight'])
+
+                for des_weight in self.freightInfo.find({'productCustomer_id': product_customer_id}):
+                    des.append(des_weight['des_weight'])
+
+            weight = 0
+            continue_weight = 0
+            account_type = 0
+            first_fee = 0
+            price = 0
+            if len(ori) != 0:
+                for i in range(0, len(ori)):
+                    if ori[i] < account_weight <= des[i]:
+                        try:
+                            account_type = self.freightInfo.find_one({'productCustomer_id': product_customer_id,
+                                                              'ori_weight': ori[i], 'des_weight': des[i]
+                                                              })['account_type']  # 结算类型
+                            first_fee = self.freightInfo.find_one({'productCustomer_id': product_customer_id, 'account_type': 1,
+                                                           'cargo_type': cargo_type})['unit_price']  # 首重价格
+                            first_weight = self.freightInfo.find_one({'productCustomer_id': product_customer_id,
+                                                              'account_type': 1, 'cargo_type': cargo_type})['des_weight']  # 首重
+                            continue_weight = account_weight - first_weight
+                        except Exception as e:
+                            print e
+
+                        if account_type == 1:  # 首重
+                            if first_fee == freight:
+                                return True
+                            else:
+                                return False
+
+                        elif account_type == 2:  # 一倍续重
+                            weight = math.ceil(continue_weight)  # 续重部分1倍向上取整
+                            continue_price = self.freightInfo.find_one({'productCustomer_id': product_customer_id,
+                                                              'cargo_type': cargo_type, 'account_type': 2
+                                                              })['unit_price']  # 1倍续重单价
+                            continue_fee = weight * continue_price  # 续重价格
+                            all_fee = first_fee + continue_fee
+                            if all_fee == freight:
+                                return True
+                            else:
+                                return False
+
+                        elif account_type == 3:  # 0.5倍续重
+
+                            multiple = (continue_weight // 1) / 0.5
+
+                            remainder = continue_weight % 1
+                            if remainder <= 0.5:
+                                multiple += 1
+                            else:
+                                multiple += 2
+
+                            continue_price = self.freightInfo.find_one({'productCustomer_id': product_customer_id,
+                                                            'cargo_type': cargo_type, 'account_type': 3})['unit_price']  # 0.5倍续重单价
+                            continue_fee = continue_price * multiple  # 续重价格
+                            all_fee = first_fee + continue_fee
+                            if all_fee == freight:
+                                return True
+                            else:
+                                return False
+
+                        elif account_type == 4:  # 单价
+                            weight = math.ceil(account_weight)  # 结算重量1倍向上取整
+                            try:
+                                price = self.freightInfo.find_one({'productCustomer_id': product_customer_id, 'account_type': 4,
+                                                           'cargo_type': cargo_type})['unit_price']  # 单价
+                            except Exception as e:
+                                print e
+                            all_fee = weight * price
+                            if all_fee == freight:
+                                return True
+                            else:
+                                return False
+
+                        elif account_type == 5:  # 固定价格
+                            price = self.freightInfo.find_one({'productCustomer_id': product_customer_id,
+                                                                  'cargo_type': cargo_type, 'account_type': 5
+                                                                  })['unit_price']  # 固定价格
+                            if price == freight:
+                                return True
+                            else:
+                                return False
+
+                    else:
+                        print weight
+            else:
+                print u'无报价'
+                if freight == 0:
+                    return True
+                else:
+                    return False
+        else:
+            print u'该站点无运费的费用配置'
+            if freight == 0:
+                return True
+            else:
+                return False
+
+    # 计算燃油附加费
+    def fuel_surcharge(self, waybill):
+        # 判断该站点是否有运费的费用配置
+        site_id = self.packageItem.find_one({'waybill_no': waybill})['current_site']
+        fuel_surcharge_config = self.chargeConfig.find_one({'site_id': site_id, 'charge_item': "燃油附加费"})
+        # 获取当前燃油附加费
+        waybill_id = self.packageItem.find_one({'waybill_no': waybill})['_id']
+        account_id = self.accountInfo.find_one({'waybill_id': waybill_id})['_id']
+        fuel_surcharge = self.charging.find_one({'account_id': account_id, 'item': '燃油附加费'})['fee']
+        correct_fuel_surcharge = 0
+
+        if not fuel_surcharge_config:
+            customer_id = self.packageItem.find_one({'waybill_no': waybill})['customer_id']
+            product_id = self.packageItem.find_one({'waybill_no': waybill})['product_id']
+            try:
+                #  产品客户有关联,取产品客户关联id
+                product_customer_id = self.productCustomer.find_one({'product_id': product_id,
+                                                            'customer_id': customer_id})['_id']
+            except Exception as e:
+                print e
+                #  产品客户无关联,取产品的默认id
+                product_customer_id = self.productCustomer.find_one({'product_id': product_id,
+                                                            'customer_id': None})['_id']
+
+            # 获取正确燃油附加费
+            try:
+                fuel_surcharge_ratio = self.productCustomer.find_one({'_id': product_customer_id})['fuel_surcharge_ratio']  # 获取燃油费率
+                if not fuel_surcharge:
+                    freight = self.charging.find_one({'account_id': account_id, 'item': '运费'})['fee']  # 获取当前运费
+                    correct_fuel_surcharge = freight * fuel_surcharge_ratio
+                    if fuel_surcharge == correct_fuel_surcharge:
+                        return True
+                    else:
+                        return False
+                else:
+                    print u'产品客户关联表中无燃油附加费率,查找产品表的默认燃油费率'
+                    fuel_surcharge_ratio = self.productInfo.find_one({'_id': product_customer_id})['fuel_surcharge_ratio']  # 获取燃油费率
+                    if not fuel_surcharge:
+                        freight = self.charging.find_one({'account_id': account_id, 'item': '运费'})['fee']  # 获取当前运费
+                        correct_fuel_surcharge = freight * fuel_surcharge_ratio
+                        if fuel_surcharge == correct_fuel_surcharge:
+                            return True
+                        else:
+                            return False
+                    else:
+                        correct_fuel_surcharge = None
+                        if fuel_surcharge == correct_fuel_surcharge:
+                            return True
+                        else:
+                            return False
+            except Exception as e:
+                print e
+
+        else:
+            print u'该站点无燃油附加费的费用配置'
+            freight = self.charging.find_one({'account_id': account_id, 'item': '运费'})['fee']  # 获取当前运费
+            correct_fuel_surcharge = None
+            if fuel_surcharge == correct_fuel_surcharge:
+                return True
+            else:
+                return False
+
+
+
+
+
+
+
+
 
 
